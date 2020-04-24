87d4dc82ccc2434d9e9cf871f9e1c7db90693394
Easton <contagon6@gmail.com>
2020-04-24 12:34:22 -0600

    Try again for package versions

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        "tensorflow >= 2.1",
+        "tensorflow",
         "gdspy >= 1.5.2",
         "joblib",
         "scikit-rf"
