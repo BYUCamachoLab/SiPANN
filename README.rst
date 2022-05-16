e55034f6593af2eb0756a86eea697da6e665f222
Sequoia Ploeg <sequoiap4@gmail.com>
2022-05-16 10:42:47 -0600

    Bump version: 1.3.1 → 1.3.2

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 **************************
-SiPANN 1.3.1
+SiPANN 1.3.2
 **************************
 .. image:: https://img.shields.io/pypi/v/SiPANN.svg
    :target: https://pypi.python.org/pypi/SiPANN
--- SiPANN/__init__.py
+++ SiPANN/__init__.py
@@ -5,6 +5,6 @@ We leverage various machine learning techniques to simulate integrated
 photonic device circuits.
 """
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 __author__ = "Easton Potokar, Alec Hammond, R Scott Collings <eastonpost@byu.edu>"
 __all__ = []
